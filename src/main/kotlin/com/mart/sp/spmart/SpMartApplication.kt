d27005c715d3470dff2abbd48c9c76272bed8ddc
박소정 <psj6414@naver.com>
2021-03-21 19:15:55 +0900

    add

---
+++ src/main/kotlin/com/mart/sp/spmart/SpMartApplication.kt
@@ -0,0 +1,11 @@
+package com.mart.sp.spmart
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpMartApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpMartApplication>(*args)
+}
---
+++ src/test/kotlin/com/mart/sp/spmart/SpMartApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mart.sp.spmart
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpMartApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
