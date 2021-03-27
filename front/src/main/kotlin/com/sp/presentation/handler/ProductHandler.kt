0e62fdd5e87a95c11c70ec8ad940b02d58a0014e
So Jeong Park <psj6414@naver.com>
2021-03-27 14:08:56 +0900

    modify: repository, productHandler

--- front/src/main/kotlin/com/sp/presentation/handler/ProductHandler.kt
+++ front/src/main/kotlin/com/sp/presentation/handler/ProductHandler.kt
@@ -1,19 +1,18 @@
 package com.sp.presentation.handler
 
-import com.sp.presentation.repository.ProductRepository
-import kotlinx.coroutines.FlowPreview
+import com.sp.repository.ProductRepository
 import org.springframework.stereotype.*
 import org.springframework.web.reactive.function.server.*
 @Component
 class ProductHandler (
     private var productRepository: ProductRepository
 ) {
-    @FlowPreview
+    //@FlowPreview
     suspend fun findAll(request: ServerRequest): ServerResponse =
         ServerResponse.ok().json().bodyValueAndAwait(productRepository.findAll())
 
     suspend fun findByTitle(request: ServerRequest): ServerResponse {
-        val title = request.pathVariable("title").toString()
+        val title = request.pathVariable("title")
         return ServerResponse.ok().json().bodyValueAndAwait(productRepository.findByTitle(title))
     }
 }
--- front/src/main/kotlin/com/sp/repository/ProductRepository.kt
+++ front/src/main/kotlin/com/sp/repository/ProductRepository.kt
@@ -1,9 +1,8 @@
-package com.sp.presentation.repository
+package com.sp.repository
 
 import com.sp.presentation.model.Product
 import org.springframework.data.jpa.repository.JpaRepository
 import reactor.core.publisher.*
-import java.util.concurrent.Flow
 
 interface ProductRepository : JpaRepository<Product, Long>{
     override fun findAll(): MutableList<Product>
